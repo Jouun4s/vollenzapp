ab5ac6aaacac71f94c6a6e5f6487e70f671c752e
joaopereira <jounasssjapa378@gmail.com>
2024-02-14 17:28:43 -0300

    estilização dados section dados

--- src/app/page-medidas/section-imc/section-imc.component.ts
+++ src/app/page-medidas/section-imc/section-imc.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { SliderModule } from 'primeng/slider';
 
 @Component({
   selector: 'app-section-imc',
   standalone: true,
-  imports: [SliderModule],
+  imports: [SliderModule,FormsModule],
   templateUrl: './section-imc.component.html',
   styleUrl: './section-imc.component.scss'
 })
 export class SectionImcComponent {
+  value!: number;
 
 }
+
