fa1add6abb06b60017b84c798139406b3ef1f9a1
joaopereira <jounasssjapa378@gmail.com>
2024-02-20 17:14:37 -0300

    centralização histórico

--- src/app/page-historico/section-historico-imc/section-historico-imc.component.ts
+++ src/app/page-historico/section-historico-imc/section-historico-imc.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { TableModule } from 'primeng/table';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-section-historico-imc',
   standalone: true,
-  imports: [TableModule],
+  imports: [TableModule,CardModule,DividerModule],
   templateUrl: './section-historico-imc.component.html',
   styleUrl: './section-historico-imc.component.scss'
 })
