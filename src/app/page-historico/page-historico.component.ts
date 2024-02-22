45b72bf2c8c9c876ea72a14f63fafb893877ec6e
joaopereira <jounasssjapa378@gmail.com>
2024-02-22 17:58:55 -0300

    Terminação front-end

--- src/app/page-historico/page-historico.component.ts
+++ src/app/page-historico/page-historico.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { SectionAcontComponent } from './section-acont/section-acont.component';
 import { SectionHistoricoImcComponent } from './section-historico-imc/section-historico-imc.component';
-import { SectionHistoricoPesoComponent } from './section-historico-peso/section-historico-peso.component';
+
 
 
 @Component({
   selector: 'app-page-historico',
   standalone: true,
-  imports: [SectionAcontComponent,SectionHistoricoImcComponent,SectionHistoricoPesoComponent],
+  imports: [SectionAcontComponent,SectionHistoricoImcComponent,],
   templateUrl: './page-historico.component.html',
   styleUrl: './page-historico.component.scss'
 })
