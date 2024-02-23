c186ae55561518df5f2d6a976877a11ae59e02fc
joaopereira <jounasssjapa378@gmail.com>
2024-02-23 14:50:44 -0300

    fuinalização front-end

--- src/app/page-medidas/section-imc/section-imc.component.ts
+++ src/app/page-medidas/section-imc/section-imc.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SliderModule } from 'primeng/slider';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-section-imc',
   standalone: true,
-  imports: [SliderModule,FormsModule],
+  imports: [SliderModule,FormsModule,DividerModule],
   templateUrl: './section-imc.component.html',
   styleUrl: './section-imc.component.scss'
 })
--- src/app/page-medidas/section-peso/section-peso.component.ts
+++ src/app/page-medidas/section-peso/section-peso.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SliderModule } from 'primeng/slider';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-section-peso',
   standalone: true,
-  imports: [SliderModule],
+  imports: [SliderModule,DividerModule],
   templateUrl: './section-peso.component.html',
   styleUrl: './section-peso.component.scss'
 })
